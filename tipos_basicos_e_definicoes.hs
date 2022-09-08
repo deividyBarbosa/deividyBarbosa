aa412336ad7a6e66c284fa7319b9b250a6d8d41a
Deividy Barbosa <deividysb17@gmail.com>
2022-09-08 17:15:44 -0300

    commit test

---
+++ tipos_basicos_e_definicoes.hs
@@ -0,0 +1,5 @@
+-- dados dois inteiros, retornar o triplo da divisão inteira destes números 
+triploDiv :: Int -> Int -> Int 
+triploDiv a b = (a+b) `div` 2 * 3
+
+-- dados dois caracteres, retornar o menor deles de acordo com a ordem alfabética 
